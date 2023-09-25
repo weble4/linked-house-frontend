e47f3f3fded060abf88fd7c0f0aed758b5512323
melamond <mikisa0813@gmail.com>
2023-09-25 14:20:29 +0900

    검색결과 더미

--- src/components/HouseDetail.js
+++ src/components/HouseDetail.js
@@ -1,316 +1,215 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import bookmarkIcon from "./images/bookmark.png";
+import Hotel1 from "./images/hotel1.jpg";
+
+// 더미 데이터
+const dummyHouses = [
+    {
+        rentalId: 1,
+        description: "서울역과 가까운 호텔",
+        imagePath: Hotel1,
+        minCapacity: 2,
+        maxCapacity: 4,
+        room: 2,
+        bed: 2,
+        bathRoom: 1,
+        location: "서울, 한국",
+        reviews: [
+            {
+                customer: "John Doe",
+                title: "Great stay!",
+                content: "I had a wonderful time at this hotel. Highly recommended!",
+            },
+            {
+                customer: "Jane Smith",
+                title: "Nice place",
+                content: "The location is perfect, and the room was clean and comfortable.",
+            },
+        ],
+    },
+    // 다른 더미 데이터 추가 가능
+];
 
 const HouseDetail = () => {
-  const { rentalId } = useParams();
-  const [house, setHouse] = useState({});
-  const [imagePath, setImagePath] = useState([]);
-  const [bookmark, setBookmark] = useState();
-  const [showMore, setShowMore] = useState(false);
-  const [reviews, setReviews] = useState([]);
-  const [checkinDate, setCheckinDate] = useState("");
-  const [checkoutDate, setCheckoutDate] = useState("");
-  const [reservationNum, setReservationNum] = useState(1);
-  const [reservationSuccess, setReservationSuccess] = useState(false);
+    //    const { rentalId } = useParams();
+    const house = dummyHouses[0]; // 첫 번째 항목 가져오기
 
-  // 토큰 가져오기
-  // 만약 토큰 만료 상태인데 북마크나 예약 누를 경우 로그인 창으로 redirect 또는 로그인 하도록 유도
-  const accessToken = localStorage.getItem("accessToken");
-  const tokenExpiration = localStorage.getItem("tokenExpiration");
-  const currentTime = Date.now();
-  const [isLoggedInLocalStorage, setIsLoggedInLocalStorage] = useState(
-    !!accessToken && tokenExpiration && currentTime < tokenExpiration
-  );
+    const [imagePath] = useState(house ? [house.imagePath] : []);
+    const [bookmark, setBookmark] = useState(false); // 북마크 상태를 초기에 false로 설정
+    const [showMore, setShowMore] = useState(false);
+    const reviews = house ? house.reviews : []; // 리뷰 데이터를 더미 데이터에서 가져오도록 수정
+    const [checkinDate, setCheckinDate] = useState("");
+    const [checkoutDate, setCheckoutDate] = useState("");
+    const [reservationNum, setReservationNum] = useState(1);
+    const [reservationSuccess, setReservationSuccess] = useState(false);
 
-  console.log(bookmark);
-  if (currentTime < tokenExpiration) {
-    setIsLoggedInLocalStorage(false);
-  }
-
-  useEffect(() => {
-    const accessToken = localStorage.getItem("accessToken");
-    const fetchData = async () => {
-      try {
-        // House fetch
-        const houseResponse = await axios.get(
-          `http://localhost:8080/api/houses/${rentalId}`,
-          {
-            headers: {
-              "Content-Type": "application/json",
-              Authorization: `Bearer ${accessToken}`,
-            },
-          }
-        );
-        setHouse(houseResponse.data);
-        setImagePath(houseResponse.data.imagePath);
-
-        // Review fetch
-        const reviewsResponse = await axios.get(
-          `http://localhost:8080/api/reviews/houses/${rentalId}`,
-          {
-            withCredentials: true,
-            headers: {
-              "Content-Type": "application/json",
-              Authorization: `Bearer ${accessToken}`,
-            },
-          }
-        );
-
-        // reviewsResponse 가 배열인지 확인
-        if (Array.isArray(reviewsResponse.data)) {
-          // 배열이라면 데이터 넣기
-          setReviews(reviewsResponse.data);
-        } else {
-          // 예외 처리
-          console.error(
-            "Invalid data format for reviews:",
-            reviewsResponse.data
-          );
-        }
-        // console.log(houseResponse.data);
-        // console.log(reviewsResponse.data);
-        console.log(localStorage.getItem("id"));
-      } catch (error) {
-        console.error("Fetch Error", error);
-      }
+    // 더보기 버튼 클릭 시 이미지 더보기 활성화
+    const handleShowMore = () => {
+        setShowMore(true);
     };
-    fetchData();
-  }, [rentalId]);
 
-  // 더보기 버튼 클릭 시 이미지 더보기 활성화
-  const handleShowMore = () => {
-    setShowMore(true);
-  };
+    const handleCheckInDateTimeChange = (event) => {
+        setCheckinDate(event.target.value);
+    };
+    const handleCheckOutDateTimeChange = (event) => {
+        setCheckoutDate(event.target.value);
+    };
 
-  const addBookmark = async () => {
-    const accessToken = localStorage.getItem("accessToken");
-    try {
-      await axios.post(
-        `http://localhost:8080/api/bookmarks/${rentalId}`,
-        null,
-        {
-          withCredentials: true,
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${accessToken}`,
-          },
+    const handleGuestCountChange = (event) => {
+        setReservationNum(event.target.value);
+    };
+
+    const handleReservation = async () => {
+        try {
+            // 예약을 만들기 위한 요청 등을 이곳에 추가
+            // 성공 시 reservationSuccess를 true로 설정하면 됩니다.
+
+            setReservationSuccess(true); // 예약 성공 시
+        } catch (error) {
+            console.error("Error creating reservation", error);
+            // 예약 실패 시 에러 처리
         }
-      );
+    };
 
-      setBookmark(true);
-    } catch (error) {
-      console.error("Error adding bookmark", error);
-    }
-  };
+    const handleAddBookmark = () => {
+        setBookmark(true); // 북마크 추가
+    };
 
-  const handleCheckInDateTimeChange = (event) => {
-    setCheckinDate(event.target.value);
-  };
-  const handleCheckOutDateTimeChange = (event) => {
-    setCheckoutDate(event.target.value);
-  };
+    const ReservationSuccessPopup = ({ onClose }) => {
+        return (
+            <div className="reservation-success-popup">
+                <p>예약이 성공적으로 완료되었습니다!</p>
+                <button onClick={onClose}>닫기</button>
+            </div>
+        );
+    };
 
-  const handleGuestCountChange = (event) => {
-    setReservationNum(event.target.value);
-  };
+    const imageStyle = {
+        width: "1000px", // 이미지 너비를 500px로 고정
+        height: "500px", // 이미지 높이를 1000px로 고정
+    };
 
-  const handleReservation = async () => {
-    try {
-      // Make a POST request to create a reservation
-      const reservationData = {
-        rentalId: rentalId,
-        checkinDate: checkinDate,
-        checkoutDate: checkoutDate,
-        reservationNum: reservationNum,
-      };
-      const accessToken = localStorage.getItem("accessToken");
-      await axios.post(
-        `http://localhost:8080/api/reservations/customer/${rentalId}`,
-        reservationData,
-        {
-          withCredentials: true,
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: "Bearer " + accessToken,
-          },
-        }
-      );
-
-      setReservationSuccess(true);
-      // Handle success, e.g., show a success message or redirect to a confirmation page
-      console.log("Reservation successful!");
-    } catch (error) {
-      console.error("Error creating reservation", error);
-      // Handle errors, e.g., show an error message to the user
-    }
-  };
-
-  const ReservationSuccessPopup = ({ onClose }) => {
     return (
-      <div className="reservation-success-popup">
-        <p>예약이 성공적으로 완료되었습니다!</p>
-        <button onClick={onClose}>닫기</button>
-      </div>
-    );
-  };
-
-  return (
-    <div>
-      {/** 제목, 설명 태그 */}
-      <div className="title flex flex-col">
-        <p className="px-8 py-4 font-extrabold text-2xl">{house.description}</p>
-        <div className="flex flex-row">
-          {/** <span className="px-8 pb-4 font-semibold text-sm">설명설명{house.description}</span>*/}
-          <span className="px-8 pb-4 text-sm">호스트에게 연락하기</span>
-          {/** 로그인 되어 있을때만 북마크 표시 */}
-          {isLoggedInLocalStorage ? (
-            <button
-              className="bookmark px-8 pb-4 text-sm justify-end ml-auto"
-              onClick={addBookmark}
-            >
-              <img alt="북마크" src={bookmarkIcon} className="h-6 w-6"></img>
-            </button>
-          ) : (
-            <span></span>
-          )}
-        </div>
-      </div>
-      {/** hr 태그 */}
-      <div className="p-4">
-        <hr></hr>
-      </div>
-
-      {/** img */}
-      <div className="flex">
-        {/** 첫 번째 이미지 크게 표시 */}
-        <img
-          alt={`img0`}
-          className="px-8 pb-4 flex-1"
-          src={imagePath.length > 0 ? imagePath[0] : ""}
-        ></img>
-        <div className="flex-1">
-          {/** 나머지 이미지 리스트 작게 표시 */}
-          {showMore
-            ? imagePath
-                .slice(1)
-                .map((path, index) => (
-                  <img
-                    key={index}
-                    alt={`img${index + 1}`}
-                    className="px-8 pb-4"
-                    src={path}
-                  ></img>
-                ))
-            : imagePath
-                .slice(1, 4)
-                .map((path, index) => (
-                  <img
-                    key={index}
-                    alt={`img${index + 1}`}
-                    className="px-8 pb-4"
-                    src={path}
-                  ></img>
-                ))}
-
-          {/** 더보기 버튼 */}
-          {imagePath.length > 4 && !showMore && (
-            <button
-              onClick={handleShowMore}
-              className="px-8 pb-4 text-sm cursor-pointer text-blue-500"
-            >
-              더보기
-            </button>
-          )}
-        </div>
-        {/** 설명 */}
-        <div className="p-8 flex-4 border rounded-md mr-8">
-          <div className="flex flex-row">
-            <span>님의 방</span>
-          </div>
-          <div className="text-sm">
-            최소인원 {house.minCapacity}명 - 최대인원 {house.maxCapacity}명 -
-            침실 {house.room}개 - 침대 {house.bed}개 - 욕실 {house.bathRoom}개
-          </div>
-          <div className="text-sm">위치</div>
-          <br></br>
-          <p className="text-sm">{house.location}</p>
-        </div>
-      </div>
-      {/** hr 태그 */}
-      <div className="p-4">
-        <hr></hr>
-      </div>
-      {/** 리뷰, 예약 버튼 */}
-      <div className="flex">
-        {/** 리뷰 */}
-        {/** */}
-        <div className="flex-1">
-          {reviews ? (
-            Array.isArray(reviews) && reviews.length === 0 ? (
-              <p className="p-4">리뷰가 없습니다.</p>
-            ) : (
-              reviews.map((review) => (
-                <div key={review.feedbackCustomerId}>
-                  <h3 className="p-4">{review.customer} 님이 작성한 리뷰</h3>
-                  <div className="p-4 text-sm">{review.title}</div>
-                  <div className="p-4 text-sm">{review.content}</div>
+        <div>
+            {/** 제목, 설명 태그 */}
+            <div className="title flex flex-col">
+                <p className="px-8 py-4 font-extrabold text-2xl">{house.description}</p>
+                <div className="flex flex-row">
+                    <span className="px-8 pb-4 text-sm">호스트에게 연락하기</span>
+                    {/** 북마크 추가 버튼 */}
+                    <button className="bookmark px-8 pb-4 text-sm justify-end ml-auto" onClick={handleAddBookmark}>
+                        <img alt="북마크" src={bookmarkIcon} className="h-6 w-6"></img>
+                    </button>
                 </div>
-              ))
-            )
-          ) : (
-            <p>리뷰 정보를 불러오는 중입니다...</p>
-          )}
+            </div>
+            {/** hr 태그 */}
+            <div className="p-4">
+                <hr></hr>
+            </div>
+
+            {/** img */}
+            <div className="flex">
+                {/** 첫 번째 이미지 크게 표시 */}
+                <img alt={`img0`} className="px-8 pb-4 flex-1" src={imagePath.length > 0 ? imagePath[0] : ""} style={imageStyle}></img>
+                <div className="flex-1">
+                    {/** 나머지 이미지 리스트 작게 표시 */}
+                    {showMore
+                        ? imagePath
+                              .slice(1)
+                              .map((path, index) => (
+                                  <img key={index} alt={`img${index + 1}`} className="px-8 pb-4" src={path} style={imageStyle}></img>
+                              ))
+                        : imagePath
+                              .slice(1, 4)
+                              .map((path, index) => <img key={index} alt={`img${index + 1}`} className="px-8 pb-4" src={path}></img>)}
+
+                    {/** 더보기 버튼 */}
+                    {imagePath.length > 4 && !showMore && (
+                        <button onClick={handleShowMore} className="px-8 pb-4 text-sm cursor-pointer text-blue-500">
+                            더보기
+                        </button>
+                    )}
+                </div>
+                {/** 설명 */}
+                <div className="p-8 flex-4 border rounded-md mr-8">
+                    <div className="flex flex-row">
+                        <span>님의 방</span>
+                    </div>
+                    <div className="text-sm">
+                        최소인원 {house.minCapacity}명 - 최대인원 {house.maxCapacity}명 - 침실 {house.room}개 - 침대 {house.bed}개 - 욕실{" "}
+                        {house.bathRoom}개
+                    </div>
+                    <div className="text-sm">위치</div>
+                    <br></br>
+                    <p className="text-sm">{house.location}</p>
+                </div>
+            </div>
+            {/** hr 태그 */}
+            <div className="p-4">
+                <hr></hr>
+            </div>
+            {/** 리뷰, 예약 버튼 */}
+            <div className="flex">
+                {/** 리뷰 */}
+                <div className="flex-1">
+                    {reviews ? (
+                        reviews.length === 0 ? (
+                            <p className="p-4">리뷰가 없습니다.</p>
+                        ) : (
+                            reviews.map((review, index) => (
+                                <div key={index}>
+                                    <h3 className="p-4">{review.customer} 님이 작성한 리뷰</h3>
+                                    <div className="p-4 text-sm">{review.title}</div>
+                                    <div className="p-4 text-sm">{review.content}</div>
+                                </div>
+                            ))
+                        )
+                    ) : (
+                        <p>리뷰 정보를 불러오는 중입니다...</p>
+                    )}
+                </div>
+
+                {/** 예약 */}
+                <div className="p-8 flex-2 justify-center items-center border rounded-md mr-8">
+                    <div className="px-8 pb-4">
+                        <label className="text-sm">Check-in Date and Time:</label>
+                        <input
+                            type="datetime-local"
+                            className="w-full p-2 border rounded-md"
+                            value={checkinDate}
+                            onChange={handleCheckInDateTimeChange}
+                        />
+                    </div>
+
+                    <div className="px-8 pb-4">
+                        <label className="text-sm">Check-out Date and Time:</label>
+                        <input
+                            type="datetime-local"
+                            className="w-full p-2 border rounded-md"
+                            value={checkoutDate}
+                            onChange={handleCheckOutDateTimeChange}
+                        />
+                    </div>
+                    <div className="px-8 pb-4">
+                        <label className="text-sm">인원 수</label>
+                        <input
+                            type="number"
+                            className="w-full p-2 border rounded-md"
+                            value={reservationNum}
+                            onChange={handleGuestCountChange}
+                            min="1"
+                        />
+                    </div>
+
+                    <button onClick={handleReservation} className="w-full p-2 border rounded-md bg-lime-200 w-48 h-16">
+                        예약하기
+                    </button>
+                    {reservationSuccess && <ReservationSuccessPopup onClose={() => setReservationSuccess(false)} />}
+                </div>
+            </div>
         </div>
-
-        {/** 예약 */}
-        <div className="p-8 flex-2 justify-center items-center border rounded-md mr-8">
-          <div className="px-8 pb-4">
-            <label className="text-sm">Check-in Date and Time:</label>
-            <input
-              type="datetime-local"
-              className="w-full p-2 border rounded-md"
-              value={checkinDate}
-              onChange={handleCheckInDateTimeChange}
-            />
-          </div>
-
-          <div className="px-8 pb-4">
-            <label className="text-sm">Check-out Date and Time:</label>
-            <input
-              type="datetime-local"
-              className="w-full p-2 border rounded-md"
-              value={checkoutDate}
-              onChange={handleCheckOutDateTimeChange}
-            />
-          </div>
-          <div className="px-8 pb-4">
-            <label className="text-sm">인원 수</label>
-            <input
-              type="number"
-              className="w-full p-2 border rounded-md"
-              value={reservationNum}
-              onChange={handleGuestCountChange}
-              min="1"
-            />
-          </div>
-
-          <button
-            onClick={handleReservation}
-            className="w-full p-2 border rounded-md bg-lime-200 w-48 h-16"
-          >
-            예약하기
-          </button>
-          {reservationSuccess && (
-            <ReservationSuccessPopup
-              onClose={() => setReservationSuccess(false)}
-            />
-          )}
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default HouseDetail;
--- src/components/HouseSearchResult.js
+++ src/components/HouseSearchResult.js
@@ -1,9 +1,30 @@
-import axios from "axios";
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import Hotel1 from "./images/hotel1.jpg";
+import Hotel2 from "./images/hotel2.jpg";
+import Hotel3 from "./images/hotel3.jpg";
+
+// 더미 데이터
+const dummyHouses = [
+    {
+        rentalId: 1,
+        description: "서울역과 가까운 호텔",
+        imagePath: Hotel1, // 이미지 경로 수정
+    },
+    {
+        rentalId: 2,
+        description: "5성급 호텔",
+        imagePath: Hotel2, // 이미지 경로 수정
+    },
+    {
+        rentalId: 3,
+        description: "서울 인기있는 호텔",
+        imagePath: Hotel3, // 이미지 경로 수정
+    },
+    // 더 많은 더미 데이터 추가 가능
+];
 
 const HouseSearchResult = () => {
-    // Parameter 가져오기
     const reactLocation = useLocation();
     const searchParams = new URLSearchParams(reactLocation.search);
 
@@ -14,51 +35,30 @@ const HouseSearchResult = () => {
     const minPrice = searchParams.get("minPrice");
 
     const [houses, setHouses] = useState([]);
-    const [page, setPage] = useState(0);
-    const pageSize = useState(20);
-    console.log(page);
-    console.log(pageSize);
-    const [totalPages, setTotalPages] = useState(0);
-
-    const [isLoading, setIsLoading] = useState(true);
+    const [isLoading, setIsLoading] = useState(false); // 더미 데이터를 즉시 사용하므로 로딩 상태를 false로 설정
 
     const navigate = useNavigate();
 
-    const fetchData = useCallback(async () => {
-        try {
-            const response = await axios.get(`http://localhost:8080/api/houses`, {
-                params: {
-                    location,
-                    minPrice,
-                    maxPrice,
-                    room,
-                    bed,
-                },
-                withCredentials: true,
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            });
-
-            const { data } = response;
-            const { content, totalPages } = data;
-            setIsLoading(false);
-            setHouses(content);
-            setTotalPages(totalPages);
-            console.log(content);
-        } catch (error) {
-            console.error("Fetch Error", error);
-            setIsLoading(false);
-        }
-    }, [location, minPrice, maxPrice, room, bed]);
-
-    useEffect(() => {
-        fetchData();
-    }, [fetchData]);
-
-    const handlePageChange = (newPage) => {
-        setPage(newPage);
+    // 이미지 스타일을 지정하는 CSS
+    const imageStyle = {
+        width: "1000px", // 이미지 너비를 500px로 고정
+        height: "500px", // 이미지 높이를 1000px로 고정
     };
+
+    // 실제 API 호출 대신 더미 데이터를 사용
+    const fetchData = () => {
+        setIsLoading(true); // 데이터 로딩 시작
+
+        // 여기서는 더미 데이터를 사용하므로 별도의 HTTP 요청 없이 바로 더미 데이터를 설정
+        setHouses(dummyHouses);
+
+        setIsLoading(false); // 데이터 로딩 완료
+    };
+
+    // 컴포넌트가 마운트될 때 데이터를 가져옴
+    React.useEffect(() => {
+        fetchData();
+    }, []);
 
     return (
         <div>
@@ -71,25 +71,25 @@ const HouseSearchResult = () => {
                             <p>검색 조건에 해당하는 숙박 업소가 존재하지 않습니다.</p>
                         ) : (
                             houses.map((house) => (
-                                <li key={house.rentalId} className="p-8 m-8 border rounded-md">
+                                <li
+                                    key={house.rentalId}
+                                    className="p-8 m-8 border rounded-md list-none
+                            "
+                                >
                                     <div>
-                                        <h3>{house.description}</h3>
+                                        <h3 className="py-8 font-bold text-3xl">{house.description}</h3>
                                         <img
                                             src={house.imagePath}
-                                            alt={house.name}
+                                            alt={house.description}
+                                            style={imageStyle} // 이미지 스타일 적용
                                             onClick={() => {
-                                                navigate(`/api/houses/${house.rentalId}`);
+                                                navigate(`/houses/result`);
                                             }}
                                         />
                                     </div>
                                 </li>
                             ))
                         )}
-                        {Array.from({ length: totalPages }, (_, index) => (
-                            <button key={index} onClick={() => handlePageChange(index)} className="mx-4 my-8">
-                                {index + 1}
-                            </button>
-                        ))}
                     </>
                 )}
             </div>
